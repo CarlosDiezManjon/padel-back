a4e1180d521d393d94d5135fedb761712825214b
carlos <cdiez1995@gmail.com>
2024-01-16 21:49:05 +0100

    db to pre

--- config/db.js
+++ config/db.js
@@ -1,4 +1,7 @@
-const pgp = require('pg-promise')();
-const db = pgp('postgres://postgres:postgres@localhost:5432/padelDB');
+const pgp = require('pg-promise')()
+//const db = pgp('postgres://postgres:postgres@localhost:5432/padelDB');
+const db = pgp(
+  'postgres://padelback:QkT1PUr6uSUHK8F5aJl5imXvCl1W8l8B@dpg-cmjehqacn0vc73dmt9e0-a.frankfurt-postgres.render.com/padeldb_lhji',
+)
 
-module.exports = db;
+module.exports = db
