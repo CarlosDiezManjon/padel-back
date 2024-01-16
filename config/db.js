279af5e090bc08398874c86fba738da9d0b972c0
carlos <cdiez1995@gmail.com>
2024-01-16 21:57:14 +0100

    url database

--- config/db.js
+++ config/db.js
@@ -1,7 +1,15 @@
 const pgp = require('pg-promise')()
 //const db = pgp('postgres://postgres:postgres@localhost:5432/padelDB');
-const db = pgp(
-  'postgres://padelback:QkT1PUr6uSUHK8F5aJl5imXvCl1W8l8B@dpg-cmjehqacn0vc73dmt9e0-a.frankfurt-postgres.render.com/padeldb_lhji',
-)
+// const db = pgp(
+//   'postgres://padelback:QkT1PUr6uSUHK8F5aJl5imXvCl1W8l8B@dpg-cmjehqacn0vc73dmt9e0-a.frankfurt-postgres.render.com/padeldb_lhji',
+// )
+const db = pgp({
+  host: 'dpg-cmjehqacn0vc73dmt9e0-a.frankfurt-postgres.render.com',
+  port: 5432,
+  database: 'padeldb_lhji',
+  user: 'padelback',
+  password: 'QkT1PUr6uSUHK8F5aJl5imXvCl1W8l8B',
+  ssl: { rejectUnauthorized: false },
+})
 
 module.exports = db
